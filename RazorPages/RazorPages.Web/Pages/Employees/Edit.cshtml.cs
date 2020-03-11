441bb7ad9d4463842b4b9158acb584dbb12e32c3
FarhanKaioum <km1472233@gmail.com>
2020-03-11 22:20:56 +0600

    File upload functionality added

--- RazorPages/RazorPages.Web/Pages/Employees/Edit.cshtml.cs
+++ RazorPages/RazorPages.Web/Pages/Employees/Edit.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,10 +15,12 @@ namespace RazorPages.Web
     public class EditModel : PageModel
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public EditModel(IEmployeeRepository employeeRepository)
+        public EditModel(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
         {
             _employeeRepository = employeeRepository;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         public Employee Employee { get; set; }
@@ -36,8 +40,35 @@ namespace RazorPages.Web
 
         public IActionResult OnPost(Employee employee)
         {
+            if (Photo != null)
+            {
+                if (employee.PhotoPath != null)
+                {
+                    string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", employee.PhotoPath);
+                    System.IO.File.Delete(filePath);
+                }
+                employee.PhotoPath = ProcessUploadedFile(Photo); ;
+            }
            Employee = _employeeRepository.Update(employee);
             return RedirectToPage("Index");
+        }
+
+        // For compute photo path file name
+        private string ProcessUploadedFile(IFormFile Photo)
+        {
+            string uniqueFileName = null;
+
+            if (Photo != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using var filestream = new FileStream(filePath, FileMode.Create);
+
+                Photo.CopyTo(filestream);
+            }
+
+            return uniqueFileName;
         }
     }
 }
