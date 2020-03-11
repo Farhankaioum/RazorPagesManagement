2cb7924da8a84b7c4c5728c5da0d78af1eab48b1
FarhanKaioum <km1472233@gmail.com>
2020-03-11 22:11:34 +0600

    File upload design added

--- RazorPages/RazorPages.Web/Pages/Employees/Edit.cshtml.cs
+++ RazorPages/RazorPages.Web/Pages/Employees/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPages.Models;
@@ -19,6 +20,9 @@ namespace RazorPages.Web
         }
 
         public Employee Employee { get; set; }
+
+        [BindProperty]
+        public IFormFile Photo { get; set; }
 
         public IActionResult OnGet(int id)
         {
