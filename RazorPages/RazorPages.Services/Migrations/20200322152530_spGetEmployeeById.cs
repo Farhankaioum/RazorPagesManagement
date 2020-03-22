76fe04811a345831168306e53b6e30c94029de7e
FarhanKaioum <km1472233@gmail.com>
2020-03-22 21:29:21 +0600

    SP added

---
+++ RazorPages/RazorPages.Services/Migrations/20200322152530_spGetEmployeeById.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPages.Services.Migrations
+{
+	public partial class spGetEmployeeById : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			string procedure = @"  create proc spGetEmployeeById
+								  @Id int
+								  as
+								  begin
+									select * from Employees 
+									where Id = @Id
+								  end";
+			migrationBuilder.Sql(procedure);
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			string procedure = @"drop proc spGetEmployeeById ";
+			migrationBuilder.Sql(procedure);
+		}
+	}
+}
