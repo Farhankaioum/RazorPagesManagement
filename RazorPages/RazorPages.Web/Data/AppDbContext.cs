a3eb92a2698c9e3517b037c24afe8264e79d62b8
FarhanKaioum <km1472233@gmail.com>
2020-03-22 17:33:19 +0600

    AppDbContext class added

---
+++ RazorPages/RazorPages.Web/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPages.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPages.Web.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
